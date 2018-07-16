d014706bc6cb1dc39dc2e4214ff5da6393656689
Brett Johnson <brettthomasjohnson@gmail.com>
2018-07-16 16:31:11 -0700

    download pdf working

--- In-season Reports/2018_in_season_report/Shiny_app_2018/app.R
+++ In-season Reports/2018_in_season_report/Shiny_app_2018/app.R
@@ -3,6 +3,10 @@ library(dplyr)
 library(ggplot2)
 
 survey_seines <- readRDS("data/survey_seines.RDS")
+spp_labels <- c(CU = "Chum", PI = "Pink", SO = "Sockeye", DI = "Discovery Islands", 
+                JS = "Johnstone Strait")
+
+#######
 
 ui <- navbarPage(
   title = 'Hakai Institute Juvenile Salmon Program',
@@ -10,9 +14,7 @@ ui <- navbarPage(
            sidebarLayout(
              sidebarPanel(
                helpText(),
-               radioButtons('format', 'Document format', c('PDF', 'HTML', 'Word'),
-                            inline = TRUE),
-               downloadButton('downloadReport')
+               downloadButton("report", "Generate PDF report")
              ),
              mainPanel(
                includeMarkdown('overview.md'),
@@ -24,10 +26,10 @@ ui <- navbarPage(
            sidebarLayout(
              sidebarPanel(
                helpText(),
-               selectInput("Species", label = h3("Species"),
-                           choices = list("Sockeye" = "SO", "Pink" = "PI",
-                                          "Chum" = "CU"),
-                           selected = "SO")
+               selectInput("species", label = h3("Species"),
+                           choices = list("Sockeye" = "so_total", "Pink" = "pi_total",
+                                          "Chum" = "cu_total"),
+                           selected = "Sockeye")
              ),
              mainPanel(
                plotOutput("migration_timing")
@@ -36,31 +38,58 @@ ui <- navbarPage(
   )
 )
 
+
+################ Server
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$downloadReport <- downloadHandler(
-    filename = function() {
-      paste('my-report', sep = '.', switch(
-        input$format, PDF = 'pdf', HTML = 'html', Word = 'docx'
-      ))
-    },
-    
+  output$report <- downloadHandler(
+    # For PDF output, change this to "report.pdf"
+    filename = "report",
     content = function(file) {
-      src <- normalizePath('report.Rmd')
+      # Copy the report file to a temporary directory before processing it, in
+      # case we don't have write permissions to the current working dir (which
+      # can happen when deployed).
+      tempReport <- file.path(tempdir(), "report.Rmd")
+      file.copy("report.Rmd", tempReport, overwrite = TRUE)
       
-      # temporarily switch to the temp dir, in case you do not have write
-      # permission to the current working directory
-      owd <- setwd(tempdir())
-      on.exit(setwd(owd))
-      file.copy(src, 'report.Rmd', overwrite = TRUE)
+      # Set up parameters to pass to Rmd document
+      params <- list(species = input$species)
       
-      library(rmarkdown)
-      out <- render('report.Rmd', switch(
-        input$format,
-        PDF = pdf_document(), HTML = html_document(), Word = word_document()
-      ))
-      file.rename(out, file)
+      # Knit the document, passing in the `params` list, and eval it in a
+      # child of the global environment (this isolates the code in the document
+      # from the code in this app).
+      rmarkdown::render(tempReport, output_file = file,
+                        params = params,
+                        envir = new.env(parent = globalenv())
+      )
     }
+  )
+  output$migration_timing <- renderPlot({
+    
+    survey_seines %>% 
+      select(year, region, sampling_week, input$species) %>%
+      group_by(year, region, sampling_week) %>% 
+      summarise(mean = mean(get(input$species), na.rm = T), se = sd(get(input$species)) / sqrt(n())) %>% 
+      ungroup() %>% 
+      ggplot(aes(x = as_factor(sampling_week), y = mean, colour = region, group = region))+
+      geom_line(size = 1)+  
+      geom_point() + 
+      geom_errorbar(aes(ymin = mean - se, ymax = mean + se, width = 0.2)) +
+      scale_colour_discrete(name = "",
+                            breaks=c("DI","JS"),
+                            labels=c("Discovery Islands", "Johnstone Strait"))+
+      theme(legend.justification=c(1,0), legend.position=c(.8,.8),
+            legend.background = element_rect(fill=alpha(0.1))) + 
+      theme(legend.text = element_text(colour="black", size = 12)) +
+      theme(axis.text=element_text(size=12),
+            axis.title=element_text(size=12,face="bold")) +
+      xlab("Date") +
+      ylab("Abundance") +
+      theme(legend.title = element_blank()) +
+      theme(axis.text.x = element_text(angle = 45, vjust = 0.5)) 
+    
+  }
   )
 }
 
